2b30cbd213613b811acb3934464579969ed89d99
Anjali Yadav <145827147+Vedanjalee@users.noreply.github.com>
2025-01-13 01:11:30 +0530

    Update RemoveSpaces.java

--- RemoveSpaces.java
+++ RemoveSpaces.java
@@ -5,6 +5,6 @@ public class RemoveSpaces {
 
     public static void main(String[] args) {
         String inputStr = "hello world";
-        System.out.println(removeSpaces(inputStr));  // Output: "helloworld"
+        System.out.println(removeSpaces(inputStr));  
     }
 }
